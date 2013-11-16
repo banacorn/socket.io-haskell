395a37c125d9f021280c4ab6a8b6d82c81dcf149
banacorn <banacorn@gmail.com>
2013-11-16 19:20:38 +0800

    testing new wai-websockets

--- Web/SocketIO/Server.hs
+++ Web/SocketIO/Server.hs
@@ -1,42 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Web.SocketIO.Server where
+module Web.SocketIO.Server (server, serverConfig, defaultConfig) where
 
-import Web.SocketIO.Util
-import Web.SocketIO.Type
-import Web.SocketIO.Session
-import Web.SocketIO.Connection
-import Web.SocketIO.Request
-import Web.SocketIO.Event
+import              Web.SocketIO.Util
+import              Web.SocketIO.Type
+import              Web.SocketIO.Session
+import              Web.SocketIO.Connection
+import              Web.SocketIO.Request
+import              Web.SocketIO.Event
 
-import qualified Network.Wai as Wai
-import Network.Wai.Handler.Warp     (run)
-import Network.HTTP.Types           (status200)
-import Control.Monad.Trans          (liftIO)
+import              Control.Monad.Trans             (liftIO)
+import              Network.HTTP.Types              (status200)
+import qualified    Network.Wai                     as Wai
+import qualified    Network.Wai.Handler.Warp        as Warp
+import              Network.Wai.Handler.WebSockets  (intercept)
+import qualified    Network.WebSockets              as WS
 
 server :: Port -> SocketM () -> IO ()
 server p h = serverConfig p defaultConfig h
 
 serverConfig :: Port -> Configuration -> SocketM () -> IO ()
 serverConfig port config handler = do
+
     tableRef <- newSessionTable
 
-    run port $ \httpRequest -> liftIO $ do
+    -- intercept websocket stuffs
+    let settings = Warp.defaultSettings
+                        { Warp.settingsPort = port
+                        , Warp.settingsIntercept = intercept wsApp
+                        }
+
+    Warp.runSettings settings $ \httpRequest -> liftIO $ do
         req <- processRequest httpRequest
         response <- runConnection (Env tableRef handler config) req
         text response
 
+-- dummy test ws app
+wsApp :: WS.ServerApp
+wsApp pending = do
+    conn <- WS.acceptRequest pending
+    WS.sendTextData conn ("hello" :: Text)
 
 defaultConfig :: Configuration
 defaultConfig = Configuration {
     transports = [WebSocket, XHRPolling]
 }
 
-
 text :: Monad m => Text -> m Wai.Response
 text = return . Wai.responseLBS status200 header . fromText
-
-
 
 pollingDuration = 20
 
