c7c1c1ab66e5d7e17bc9a39ed843a02f7da153a8
Ting-Yen Lai <banacorn@gmail.com>
2013-12-03 17:19:42 +0800

    haddock

--- Web/SocketIO.hs
+++ Web/SocketIO.hs
@@ -1,16 +1,49 @@
 module Web.SocketIO
     (
+        -- * How to use this module
+        -- |
+        --
+        -- Note that most of the string literals below are of type Text.
+        --
+        -- >{-\# LANGUAGE OverloadedStrings \#-}
+        -- >
+        -- >import Web.SocketIO
+        -- >
+        -- >main = server 4000 $ do
+        -- >
+        -- >    emit "some event" ["hey"]
+        -- >
+        -- >    on "ping" $ do
+        -- >        emit "pong" []
+
         -- * Running a standalone server
-        on
+        server
+        -- | Run a socket.io application, build on top of Warp.
+    ,   serverConfig
+        -- | Run a socket.io application with configurations applied.
+    ,   defaultConfig
+        -- | Default configurations to be overridden.
+        --
+        -- > defaultConfig :: Configuration
+        -- > defaultConfig = Configuration
+        -- >    {   transports = [XHRPolling]
+        -- >    ,   logLevel = 3
+        -- >    ,   closeTimeout = 60
+        -- >    ,   pollingDuration = 20
+        -- >    }
+        --
+    ,   Configuration(..)
+    ,   Port
+        -- * Binding and triggering events
+    ,   on
     ,   emit
     ,   reply
+    ,   Event
     ,   Emittable
-        -- * Running a standalone server
-    ,   server
-    ,   serverConfig
-    ,   defaultConfig
-    ,   Configuration(..)
     ,   Transport(..)
+        -- * Types
+    ,   SocketM
+    ,   CallbackM
     ) where
 
 import Web.SocketIO.Types
