bc38196ac4f76105151a3e583bceb5498f6bd83c
Ting-Yen Lai <banacorn@gmail.com>
2014-02-22 01:34:59 +0800

    Web.SocketIO.Util haddock

--- Web/SocketIO/Util.hs
+++ Web/SocketIO/Util.hs
@@ -1,14 +1,18 @@
+--------------------------------------------------------------------------------
+-- | Exports some logging utilities and other useful functions
 {-# LANGUAGE OverloadedStrings #-}
 
 module Web.SocketIO.Util ((<>), debugLog, debugSession, debug) where
 
 --------------------------------------------------------------------------------
-import Control.Concurrent.Chan
-import Control.Monad.Trans 					(liftIO, MonadIO)
---------------------------------------------------------------------------------
 import Web.SocketIO.Types
 
 --------------------------------------------------------------------------------
+import Control.Concurrent.Chan
+import Control.Monad.Trans                  (liftIO, MonadIO)
+
+--------------------------------------------------------------------------------
+-- | Write log to channel according to log level and configurations
 debug :: (Functor m, MonadIO m, ConnectionLayer m) => (ByteString -> Log) -> ByteString -> m ()
 debug logType message = do
     logLevel' <- fmap logLevel getConfiguration
@@ -22,10 +26,12 @@ debug logType message = do
             level = levelOf log' 
 
 --------------------------------------------------------------------------------
+-- | Attaches `Web.SocketIO.Types.Base.SessionID`
 debugLog :: (Functor m, MonadIO m, ConnectionLayer m) => (ByteString -> Log) -> Session -> ByteString -> m ()
 debugLog logType (Session sessionID _ _ _ _) message = debug logType (sessionID <> "    " <> serialize message) 
 
 --------------------------------------------------------------------------------
+-- | Attaches `Web.SocketIO.Types.Base.SessionID` automatically
 debugSession :: (Functor m, MonadIO m, ConnectionLayer m, SessionLayer m) => (ByteString -> Log) -> ByteString -> m ()
 debugSession logType message = do
     Session sessionID _ _ _ _ <- getSession
