d0b457dd5084dba5615eba96078bf48bf1684879
Ting-Yen Lai <banacorn@gmail.com>
2014-02-21 23:14:13 +0800

    Web.SocketIO.Connection haddock

--- Web/SocketIO/Connection.hs
+++ Web/SocketIO/Connection.hs
@@ -1,3 +1,5 @@
+--------------------------------------------------------------------------------
+-- | Requests are comsumed and sessions are invoked at this stage.
 {-# LANGUAGE OverloadedStrings #-}
 
 module Web.SocketIO.Connection
@@ -13,11 +15,11 @@ import              Web.SocketIO.Types
 import              Web.SocketIO.Util
 
 --------------------------------------------------------------------------------
-import qualified    Data.HashMap.Strict                     as H
-import              Data.IORef.Lifted
 import              Control.Concurrent.MVar.Lifted
 import              Control.Monad.Reader       
 import              Control.Monad.Writer       
+import qualified    Data.HashMap.Strict                     as H
+import              Data.IORef.Lifted
 import              System.Random                           (randomRIO)
 
 --------------------------------------------------------------------------------
@@ -34,16 +36,19 @@ import              System.Random                           (randomRIO)
 --                  (OK)        (OK)            (OK)            (OK)
 --                  Noop        x
 --------------------------------------------------------------------------------
+-- | Initialize session table
 newSessionTableRef :: IO (IORef SessionTable)
 newSessionTableRef = newIORef H.empty
 
 --------------------------------------------------------------------------------
+-- | Session table helper function
 updateSession :: (SessionTable -> SessionTable) -> ConnectionM ()
 updateSession update = do
     tableRef <- getSessionTableRef
     liftIO (modifyIORef tableRef update)
 
 --------------------------------------------------------------------------------
+-- | Session table helper function
 lookupSession :: SessionID -> ConnectionM (Maybe Session)
 lookupSession sessionID = do
     tableRef <- getSessionTableRef
@@ -51,19 +56,24 @@ lookupSession sessionID = do
     return (H.lookup sessionID table)
 
 --------------------------------------------------------------------------------
+-- | Runs handler whenever new session comes in
 executeHandler :: HandlerM () -> ChannelHub -> SessionID -> ConnectionM [Listener]
 executeHandler handler channelHub sessionID = liftIO $ execWriterT (runReaderT (runHandlerM handler) (HandlerEnv channelHub sessionID))
 
 --------------------------------------------------------------------------------
+-- | Consumes request, hand it to the next stage, and returns its result.
 runConnection :: Env -> Request -> IO Message
 runConnection env req = do
     runReaderT (runConnectionM (handleConnection =<< (retrieveSession req))) env
 
 --------------------------------------------------------------------------------
+-- | Explicitly exposes `Web.SocketIO.Types.Base.SessionState` and make use of totality checker.
 split :: Maybe Session -> Maybe (Session, SessionState)
 split (Just session@(Session _ state _ _ _)) = Just (session, state)
 split Nothing = Nothing
 
+--------------------------------------------------------------------------------
+-- | Accesses the session table and makes session explicit
 retrieveSession :: Request -> ConnectionM (Request, Maybe (Session, SessionState))
 retrieveSession Handshake = do
     return (Handshake, Nothing)
@@ -78,16 +88,21 @@ retrieveSession (Emit sessionID e) = do
     return (Emit sessionID e, split result)
 
 --------------------------------------------------------------------------------
+-- | Big time
 handleConnection :: (Request, Maybe (Session, SessionState)) -> ConnectionM Message
 handleConnection (Handshake, _) = do
 
-
-
+    -- establish a session
     session@(Session sessionID _ _ _ _) <- makeSession
-    setTimeout session
     debugLog Debug session "[Handshake]"
+    
+    -- timeout
+    setTimeout session
 
+    -- session table management
     updateSession (H.insert sessionID session)
+
+    -- next stage
     runSession SessionHandshake session
 
 
