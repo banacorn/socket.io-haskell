422ff9ffc7152709b5689dffc79032c26c85c6bf
Ting-Yen Lai <banacorn@gmail.com>
2014-02-21 21:18:41 +0800

    Web.SocketIO.Types.String haddock

--- Web/SocketIO/Types/String.hs
+++ Web/SocketIO/Types/String.hs
@@ -1,3 +1,5 @@
+--------------------------------------------------------------------------------
+-- | String-like data structure utilities
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
@@ -10,122 +12,125 @@ module Web.SocketIO.Types.String (
     ,   Serializable(..)
     ,   Text
     ,   ByteString
-    ,   LazyText
     ,   (<>)
     ) where
+
 
 --------------------------------------------------------------------------------
 import qualified    Data.String                             as S
 import qualified    Data.Text                               as T
+import              Data.Text                               (Text)
 import qualified    Data.Text.Lazy                          as TL
-import qualified    Data.ByteString                         as B
+import              Data.ByteString                         (ByteString)
 import qualified    Data.ByteString.Lazy                    as BL
 import qualified    Data.ByteString.Char8                   as C
 import              Data.Monoid                             ((<>), Monoid)
 
---------------------------------------------------------------------------------
-type Text = T.Text
-type LazyText = TL.Text
-type ByteString = B.ByteString
 
 --------------------------------------------------------------------------------
+-- | Class for string-like data structures that can be converted from strict ByteString
 class IsByteString a where
     fromByteString :: ByteString -> a
 
---------------------------------------------------------------------------------
+-- | to String
 instance IsByteString String where
     fromByteString = C.unpack
 
---------------------------------------------------------------------------------
+-- | to strict Text
 instance IsByteString T.Text where
     fromByteString = T.pack . fromByteString
 
---------------------------------------------------------------------------------
+-- | to lazy Text
 instance IsByteString TL.Text where
     fromByteString = TL.pack . fromByteString
 
---------------------------------------------------------------------------------
+-- | to strict ByteString (identity)
 instance IsByteString ByteString where
     fromByteString = id
 
---------------------------------------------------------------------------------
+-- | to lazy ByteString
 instance IsByteString BL.ByteString where
     fromByteString = BL.fromStrict
 
 --------------------------------------------------------------------------------
+-- | Class for string-like data structures that can be converted from lazy ByteString
 class IsLazyByteString a where
     fromLazyByteString :: BL.ByteString -> a
 
---------------------------------------------------------------------------------
+-- | to String
 instance IsLazyByteString String where
     fromLazyByteString = fromByteString . BL.toStrict
 
---------------------------------------------------------------------------------
+-- | to strict Text
 instance IsLazyByteString T.Text where
     fromLazyByteString = fromByteString . BL.toStrict
 
---------------------------------------------------------------------------------
+-- | to lazy Text
 instance IsLazyByteString TL.Text where
     fromLazyByteString = fromByteString . BL.toStrict
 
---------------------------------------------------------------------------------
+-- | to strict ByteString
 instance IsLazyByteString ByteString where
     fromLazyByteString = BL.toStrict
 
---------------------------------------------------------------------------------
+-- | to lazy ByteString (identity)
 instance IsLazyByteString BL.ByteString where
     fromLazyByteString = id
 
 --------------------------------------------------------------------------------
+-- | Class for string-like data structures that can be converted from strict Text
 class IsText a where
     fromText :: T.Text -> a
 
---------------------------------------------------------------------------------
+-- | to String
 instance IsText String where
     fromText = T.unpack
 
---------------------------------------------------------------------------------
+-- | to strict Text (identity)
 instance IsText Text where
     fromText = id
 
---------------------------------------------------------------------------------
+-- | to lazy Text
 instance IsText TL.Text where
     fromText = TL.fromStrict
 
---------------------------------------------------------------------------------
+-- | to strict ByteString
 instance IsText ByteString where
     fromText = C.pack . fromText
 
---------------------------------------------------------------------------------
+-- | to lazy ByteString
 instance IsText BL.ByteString where
     fromText = BL.fromStrict . C.pack . fromText
 
 --------------------------------------------------------------------------------
+-- | Class for string-like data structures that can be converted from lazy Text
 class IsLazyText a where
     fromLazyText :: TL.Text -> a
 
---------------------------------------------------------------------------------
+-- | to String
 instance IsLazyText String where
     fromLazyText = TL.unpack
 
---------------------------------------------------------------------------------
+-- | to strict Text
 instance IsLazyText T.Text where
     fromLazyText = TL.toStrict
 
---------------------------------------------------------------------------------
+-- | to lazy Text (identity)
 instance IsLazyText TL.Text where
     fromLazyText = id
 
---------------------------------------------------------------------------------
+-- | to strict ByteString
 instance IsLazyText ByteString where
     fromLazyText = C.pack . fromLazyText
 
---------------------------------------------------------------------------------
+-- | to lazy ByteString
 instance IsLazyText BL.ByteString where
     fromLazyText = BL.fromStrict . C.pack . fromLazyText
 
 --------------------------------------------------------------------------------
+-- | Class for string-like data structures
 class Serializable a where
+    -- | converts instances to string-like data structures
     serialize :: ( Monoid s
                  , S.IsString s
                  , IsText s
