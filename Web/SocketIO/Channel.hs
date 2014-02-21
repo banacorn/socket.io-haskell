a45e3130c0b6662abf243598845e6f0094df313f
Ting-Yen Lai <banacorn@gmail.com>
2014-02-21 22:27:57 +0800

    Web.SocketIO.Channel haddock

--- Web/SocketIO/Channel.hs
+++ Web/SocketIO/Channel.hs
@@ -1,3 +1,5 @@
+--------------------------------------------------------------------------------
+-- | Unbounded FIFO channel for sessions
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 
@@ -22,20 +24,25 @@ import              Control.Monad.Base                      (MonadBase)
 import              Control.Monad.Trans.Control             (MonadBaseControl)
 import qualified    Data.ByteString.Char8                   as BC
 import              System.IO                               (Handle, stderr)
+
 --------------------------------------------------------------------------------
+-- | Server-wide cross-session channel for broadcasting.
 newGlobalChannel :: MonadBase IO m => m (Chan Package)
 newGlobalChannel = newChan
 
 --------------------------------------------------------------------------------
+-- | Server-wide cross-session channel for logging.
 newLogChannel :: MonadBase IO m => m (Chan ByteString)
 newLogChannel = newChan
 
 --------------------------------------------------------------------------------
+-- | Forks a thread that sucks stuffs from a channel to a handle forever.
 streamToHandle :: Handle -> Chan ByteString -> IO ()
 streamToHandle handle channel = void . fork . forever $ do
     readChan channel >>= BC.hPutStrLn handle
 
 --------------------------------------------------------------------------------
+-- | Initialize a collection of channels for a new `Web.SocketIO.Types.Base.Session`
 makeChannelHub :: SessionID -> ConnectionM ChannelHub
 makeChannelHub sessionID = do
 
@@ -53,6 +60,7 @@ makeChannelHub sessionID = do
     return $ ChannelHub localChannel globalChannelClone outputChannel logChannel
 
 ------------------------------------------------------------------------------
+-- | Merges both local and global (broadcast) data to output channel.
 streamBothChannelTo :: (MonadBaseControl IO m, MonadBase IO m) => SessionID -> Chan Package -> Chan Package -> Chan Package -> m ()
 streamBothChannelTo sessionID local global output = do
     -- local
